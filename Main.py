3316245afbd5c534dc071d0e72eb06518572bc3f
Luko van der Maas <lukovandermaas@gmail.com>
2014-05-30 21:26:16 +0200

    changed some things and finished the aplication

--- Main.py
+++ Main.py
@@ -5,7 +5,10 @@ from pygame.locals import *
 pygame.init()
 pygame.font.init()
 
-screen = pygame.display.set_mode((500, 200))
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
+
+screen = pygame.display.set_mode((270, 200))
 pygame.display.set_caption('Filter the light')
 
 background = pygame.Surface(screen.get_size())
@@ -60,9 +63,10 @@ clock = pygame.time.Clock()
 run = True
 value = 0
 cuHo = [0, 0]
-schuif = {"value": 0.0, "rect": pygame.Rect(123, 50, 30, 20), "click": False}
+schuif = {"value": 0.0, "rect": pygame.Rect(108, 50, 30, 20), "click": False}
 valSrf = pygame.Surface((16, 20)).convert_alpha()
 font = pygame.font.Font(None, 20)
+light = pygame.image.load("Light.png")
 
 cuvets = pygame.sprite.RenderPlain(Cuvet(10, "A", (85, 170)), Cuvet(-10, "B", (115, 170)), Cuvet(1, "C", (145, 170)), Cuvet(-1, "D", (175, 170)), Cuvet(10, "A", (85, 170)), Cuvet(-10, "B", (115, 170)), Cuvet(1, "C", (145, 170)), Cuvet(-1, "D", (175, 170)))
 
@@ -112,17 +116,21 @@ while run:
     pygame.draw.line(screen, (0, 0, 0), (20, 60), (226, 60), 5)
     schuifText1 = font.render(u"-90°", True, (0, 0, 0))
     schuifText2 = font.render(u"90°", True, (0, 0, 0))
+    schuifText3 = font.render(u"0°", True, (0, 0, 0))
+    schuifTextFollow = font.render(unicode(int(schuif["value"])) + u"°", True, (0, 0, 0))
+    pygame.draw.rect(screen, (255, 208, 69), schuif["rect"])
     screen.blit(schuifText1, (12, 35))
     screen.blit(schuifText2, (220, 35))
-    pygame.draw.rect(screen, (255, 208, 69), schuif["rect"])
+    screen.blit(schuifText3, (118, 35))
+    screen.blit(schuifTextFollow, (schuif["rect"].centerx - 10, schuif["rect"].centery - 5))
 
     #drawing other stuff
-    pygame.draw.rect(screen, (222, 0, 0), pygame.Rect(20, 90, 20, 20), 0)
+    screen.blit(light, (20, 83))
     pygame.draw.lines(screen, (0, 0, 0), True, ((70, 130), (70, 110), (75, 110), (75, 90), (65, 90), (65, 110), (70, 110)), 3)
     pygame.draw.lines(screen, (0, 0, 0), False, ((101, 110), (101, 120), (124, 120), (124, 110)), 3)
     pygame.draw.lines(screen, (0, 0, 0), False, ((131, 110), (131, 120), (154, 120), (154, 110)), 3)
     pygame.draw.lines(screen, (0, 0, 0), True, ((180, 130), (180, 110), (185, 110), (185, 90), (175, 90), (175, 110), (180, 110)), 3)
-    valSrf.fill((255, 0, 0, value * 255))
+    valSrf.fill((0, 255, 0, value * 255))
     screen.blit(valSrf, (201, 90))
     displayScreen = pygame.draw.lines(screen, (0, 0, 0), True, ((210, 130), (210, 110), (218, 110), (218, 90), (202, 90), (202, 110), (210, 110)), 3)
 
